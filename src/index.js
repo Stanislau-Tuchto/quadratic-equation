4fd1c1a5fc4d32375af32d0b621cee840c1055f7
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-02-25 01:16:17 +0300

    implement solveEquation 70ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  console.log(equation);
+    var a,b,c,D,x=[];
+    //Todo: prettier regexp
+    equation = equation.replace( "* x^2", "" ).replace( "* x", "" ).replace( /[+]\s/g, "+" ).replace( /[-]\s/g, "-" ).split(/\s/g);
+    a = +equation[0];
+    b = +equation[2];
+    c = +equation[4];
+    D = b*b-4*a*c;
+    if(D >= 0){
+        x[0] = Math.round((-b + Math.sqrt(D))/(2*a));
+        x[1] = Math.round((-b - Math.sqrt(D))/(2*a));
+        return x.sort((a,b) => a - b);
+    }else return null;
+
 }
